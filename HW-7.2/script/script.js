33dae1c825e103202be8d527b738448fd4899709
Dmytro Stamov <statik180@gmail.com>
2024-06-11 13:30:55 +0300

    finised HW-7.2

---
+++ HW-7.2/script/script.js
@@ -0,0 +1,15 @@
+const curFunction = (sum) => {
+	return function(a) {
+		return function(b) {
+			return sum(a, b)
+		}
+	}
+}
+
+const sum = (a, b) => {
+	return a + b
+}
+
+let curSum = curFunction(sum)
+
+console.log(curSum(1)(2))
