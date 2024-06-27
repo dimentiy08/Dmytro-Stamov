29757e3e02ea2e932b0a860653a6c8c3a76a5eaf
Dmytro Stamov <statik180@gmail.com>
2024-06-27 16:11:51 +0300

    fixed HW-11.1 task and fixed typo

--- HW-11.1/script/script.js
+++ HW-11.1/script/script.js
@@ -1,21 +1,3 @@
-
-// for (let i = 1; i <= 10; i++) {
-// 	const newDiv = document.createElement('div')
-// 	let factor = 10
-// 	let result = i * factor
-// 	newDiv.textContent = `${i} * 10 = ${result}` 
-// 	const parentElement = document.querySelector('.container');
-// 	parentElement.appendChild(newDiv)
-// }
-
-// let getBodyElement = document.getElementsByTagName("body")[0]
-// let pythagoreanCell = document.createElement("div")
-
-
-// getBodyElement.append(pythagoreanCell)
-// pythagoreanCell.textContent = 'Hello'
-// pythagoreanCell.classList.add('box')
-
 document.addEventListener('DOMContentLoaded', function() {
 	const getBodyTag = document.getElementsByTagName("body")[0]
 	getBodyTag.classList.add("body")
