31e53cabd333c9d2c8bffe87fcebfb35734dc96d
Dmytro Stamov <statik180@gmail.com>
2024-07-22 23:54:55 +0300

    finished 14.1 task

--- HW-14.1/script/script.js
+++ HW-14.1/script/script.js
@@ -1,69 +1,114 @@
 
+
 document.addEventListener('DOMContentLoaded', function () {
-	const carusel = document.querySelector('.carusel')
+
 	const nextButton = document.querySelector('.next')
 	const prevButton = document.querySelector('.prev')
-	const bulletContainer = document.createElement('div')
-	bulletContainer.classList.add('bullet-container')
-	carusel.append(bulletContainer)
+	const allSlides = document.querySelectorAll('.slide')
+	const carusel = document.querySelector('.carusel')
+	const circleContainer = document.createElement('div')
+	circleContainer.classList.add('circle-container')
+	carusel.append(circleContainer)
 
-	const changeingImages = (offset) => {
-			const activeSlide = document.querySelector('[data-active]')
-			const allSlides = [...document.querySelectorAll('.slide')]
-			const currentIndex = allSlides.indexOf(activeSlide)
+
+
+	allSlides.forEach((elem, index) => {
+		elem.setAttribute('id', index)
+	})
+	
+	allSlides.forEach((_, index) => {
+		const elem = document.createElement('label')
+		elem.classList.add(`circle-${index}`, 'circle' )
+		circleContainer.append(elem)
+		elem.setAttribute('for', index)
+	})
+
+	const elem = document.querySelectorAll('.circle')
+	
+	const circleElem = (forNum) => {
+
+		elem.forEach((el) => {
+		const activeSlide = document.querySelector('[data-active]')
+		el.classList.remove('active')
+		elem[forNum].classList.add('active')
+
+		delete activeSlide.dataset.active
+		allSlides[forNum].dataset.active = true
+		})
+	}
+
+	const nextPhoto = (offset) => {
+		const allSlides = [...document.querySelectorAll('.slide')]
+		const activeSlide = document.querySelector('[data-active]')
+		allSlides.forEach(() => {
+			let currentIndex = allSlides.indexOf(activeSlide)
 			let newIndex = currentIndex + offset
+			
+			if (newIndex < 0) {
+				newIndex = allSlides.length -1
+			} 
 
-			if (newIndex < 0) newIndex = allSlides.length - 1
-			if (newIndex >= allSlides.length) newIndex = 0
+			if (currentIndex === allSlides.length -1) {
+				document.getElementById("next").disabled = true;
+			} else if(currentIndex >= 0) {
+				document.getElementById("prev").disabled = false;
+			}
 
 			allSlides[newIndex].dataset.active = true
 			delete activeSlide.dataset.active
 
-			updateBullets(newIndex)
+			if (newIndex < allSlides.length) {
+				circleElem(newIndex)
+		}
+		})
 	}
 
-	const bulletCreator = () => {
-			const allSlides = document.querySelectorAll('.slide')
 
-			allSlides.forEach((_, index) => {
-					const bullet = document.createElement('div')
-					bullet.classList.add('bullet')
-					bullet.dataset.index = index
-					bullet.addEventListener('click', () => goToSlide(index))
-					bulletContainer.append(bullet)
-			})
+	const previeousPhoto = (offset) => {
+		const allSlides = [...document.querySelectorAll('.slide')]
+		const activeSlide = document.querySelector('[data-active]')
+		allSlides.forEach(() => {
+			let currentIndex = allSlides.indexOf(activeSlide)
+			let newIndex = currentIndex - offset
+			
+			if (newIndex >= allSlides.length) {
+				newIndex = 0
+			}
 
-			updateBullets(0)
-	}
+			if (currentIndex === 0) {
+				document.getElementById("prev").disabled = true;
+			} else if (currentIndex <= allSlides.length -1) {
+				document.getElementById("next").disabled = false;
+			}
 
-	const updateBullets = (activeIndex) => {
-			const allBullets = document.querySelectorAll('.bullet')
-			allBullets.forEach((bullet, index) => {
-					if (index === activeIndex) {
-							bullet.classList.add('active')
-					} else {
-							bullet.classList.remove('active')
-					}
-			})
-	}
-
-	const goToSlide = (index) => {
-			const activeSlide = document.querySelector('[data-active]')
-			const allSlides = [...document.querySelectorAll('.slide')]
-
-			allSlides[index].dataset.active = true
+			allSlides[newIndex].dataset.active = true
 			delete activeSlide.dataset.active
+			
 
-			updateBullets(index)
+
+			if (newIndex >= 0) {
+				circleElem(newIndex);
+		}
+		})
 	}
 
-	bulletCreator()
+	carusel.addEventListener('click', (e) => {
 
-	nextButton.addEventListener('click', () => {
-			changeingImages(1)
+		if(e.target === nextButton) {
+			nextPhoto(1)
+		}
+
+		if(e.target === prevButton) {
+			previeousPhoto(1)
+		}
+
+		if(e.target) {
+			const forNum = e.target.attributes.for.nodeValue
+			circleElem(forNum)
+		}
 	})
 
-	prevButton.addEventListener('click', () => {
-			changeingImages(-1)
-	})
+	const initialIndex = [...allSlides].findIndex(slide => slide.hasAttribute('data-active'))
+	elem[initialIndex].classList.add('active')
 })
+
